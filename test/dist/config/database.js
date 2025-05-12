01f6c9ce32a788d68ad1e917a4ce3540eb65eea2
Felipe Zeferino <112906727+FelipeZeferino@users.noreply.github.com>
2025-05-12 17:08:21 -0300

    Delete test directory

--- test/dist/config/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const better_sqlite3_1 = __importDefault(require("better-sqlite3"));
-const db = new better_sqlite3_1.default('foobar.db');
-db.pragma('journal_mode = WAL');
--- test/dist/routes/routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get('/');
-//routes.get('/user', UserController)
